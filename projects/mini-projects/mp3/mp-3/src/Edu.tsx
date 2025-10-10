0f076c2797a4eddefc4ae7f1e43826ffca4fc504
timmyboidk <zaiyangyu369@gmail.com>
2025-10-10 12:25:59 -0400

    mp3

---
+++ projects/mini-projects/mp3/mp-3/src/Edu.tsx
@@ -0,0 +1,20 @@
+export default function Edu(){
+    return (
+        <main>
+            <section>
+                <h2>Boston University - Boston, MA</h2>
+                <p><strong>Bachelor in Computer Science, Minor in Psychology</strong> (Expected May 2026)</p>
+                <p>GPA: 3.7 </p>
+                <h3>Relevant Course Work:</h3>
+                <ul>
+                    <li>Data Structure and Algorithms</li>
+                    <li>Analysis of Algorithms</li>
+                    <li>Foundations of Data Science</li>
+                    <li>Computer Organization</li>
+                    <li>Intro to Web Security</li>
+                    <li>Mobile App Development</li>
+                </ul>
+            </section>
+        </main>
+    )
+}
---
+++ projects/mini-projects/mp3/mp-3/src/Experience.tsx
@@ -0,0 +1,24 @@
+export default function Experience() {
+    return (
+        <main>
+            <section>
+                <article>
+                    <h2>Yicheng Tech - Boston, MA</h2>
+                    <p><strong>Chief Technology Officer & Co-founder</strong> (May 2024 - Present)</p>
+                    <ul>
+                        <li>Defined a microservices architecture with Spring Boot, implemented backend service and oversaw testing, and deployed the system on AWS.</li>
+                        <li>Translated UI/UX designs into an iOS app using Swift, developed a Node.js web interface, managed deployment, and guided the Apple Store approval process.</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Caro Tech Co. - Shanghai, China</h2>
+                    <p><strong>Internship</strong> (May 2023 - July 2023)</p>
+                    <ul>
+                        <li>Contributed to an AI-driven road inspection platform by cleaning labeled image sets, testing model training, validating detection accuracy, visualizing results, and preparing daily reports for supervisors.</li>
+                        <li>Participated in refactoring the development stack for improved distributed computing capabilities and the deployment of models for data processing and real-time road damage detection.</li>
+                    </ul>
+                </article>
+            </section>
+        </main>
+    )
+}
