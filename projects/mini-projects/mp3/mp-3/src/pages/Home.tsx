d484fb0c075a5f11515de0c93e41608568e97f7f
timmyboidk <zaiyangyu369@gmail.com>
2025-10-21 22:39:52 -0400

    fixed having white space at the bottom

--- projects/mini-projects/mp3/mp-3/src/pages/Home.tsx
+++ projects/mini-projects/mp3/mp-3/src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Main = styled.main`
     width: 70%;
     padding: 30px;
     background-color: #ffffff;
-    min-height: calc(100vh);
+    min-height: 100vh;
     flex-grow: 1;
     p, ul {
         line-height: 1.7;
