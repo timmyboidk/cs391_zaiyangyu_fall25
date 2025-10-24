f16f503af7c31f5886f2fce040a1c26ee848d9e6
timmyboidk <zaiyangyu369@gmail.com>
2025-10-24 13:53:52 -0400

    dis-7

---
+++ Discussions/dis-7/dis-7/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage(){
+    return (
+        <div
+        className="flex flex-col justify-center items-center bg-red-300 p-2">
+            <h1>I am a CS391 student</h1>
+            <p>This is one of my favourite classes</p>
+        </div>
+    );
+}
